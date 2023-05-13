df5b0fbba28c6312cee54ed5532324342750efc7
Hasan Huda <hasanhuda037@gmail.com>
2023-05-13 13:22:57 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StyleSheet, View } from "react-native";
+
+import StartGameScreen from "./screens/StartGameScreen";
+
+export default function App() {
+  return (
+    <View style={styles.rootContainer}>
+      <StartGameScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    backgroundColor: "#ddb52f",
+    flex: 1,
+  },
+});
